01b92223538c8b4e9a8fe6871e298a5b395cc84e
Stephane Moser <moser.sts@gmail.com>
2019-06-21 01:39:06 +0100

    Refactor git client to be more easy to use with commit manager

--- src/clients/git.js
+++ src/clients/git.js
@@ -1,88 +1,103 @@
 const util = require('util');
 const fs = require('fs');
 const exec = util.promisify(require('child_process').exec);
-const REPO_STORAGE = '/tmp/repos'
+const REPO_STORAGE = '/tmp/repos';
 const _ = require('lodash');
 
 const sendLog = (output) => {
   if(! _.isEmpty(output)){
-    console.log(`Result: ${output.trim()}`)
+    console.log(`Result: ${output.trim()}`);
   }
-}
+};
 
-async function clone(org, repository) {
-  const repoFullname = `${org}/${repository}`
+async function clone(repoPath, projectName) {
   try {
-    const {stdout, stderr} = await exec(`git clone --progress https://github.com/${repoFullname}.git ${REPO_STORAGE}/${repoFullname}`);
-    console.log(`Repository ${repoFullname} cloned with success`)
-    sendLog(stdout)
-    sendLog(stderr)
+    const {stdout, stderr} = await exec(`git clone --progress https://github.com/${projectName}.git ${repoPath}/${projectName}`);
+    console.log(`Repository ${projectName} cloned with success`);
+    sendLog(stdout);
+    sendLog(stderr);
 
   } catch (error) {
-    console.warn(`Clone repository ${repoFullname} failed`)
-    console.warn(`Reasion for failure : ${error.stderr}`)
-    throw error
+    console.warn(`Clone repository ${projectName} failed`);
+    console.warn(`Reasion for failure : ${error.stderr}`);
+    throw error;
   }
 
 }
-async function pull(org, repository) {
-  const repoFullname = `${org}/${repository}`
+async function pull(rootPath, projectName) {
   try {
-    const {stdout, stderr} = await exec(`git pull`,{
-      cwd: `${REPO_STORAGE}/${repoFullname}`
+    const {stdout, stderr} = await exec(`git pull origin master`,{
+      cwd: `${rootPath}/${projectName}`
     });
-    sendLog(stdout)
-    sendLog(stderr)
+    sendLog(stdout);
+    sendLog(stderr);
   } catch (error) {
-    console.warn(`Pull repository ${repoFullname} failed`)
-    console.warn(`Reasion for failure : ${error.stderr}`)
-    throw error
+    console.warn(`Pull project ${projectName} failed`);
+    console.warn(`Reasion for failure : ${error.stderr}`);
+    throw error;
   }
 }
 
-async function log(org, repository) {
-  const repoFullname = `${org}/${repository}`
+async function log(rootPath, projectName) {
   try {
     const {stdout} = await exec('git log --date=iso8601-strict --pretty=format:"%H_%an_%ad_%s"',{
-      cwd: `${REPO_STORAGE}/${repoFullname}`
+      cwd: `${rootPath}/${projectName}`
     });
 
-    console.log(`Commits log obtained with sucess for repository ${repoFullname}`)
-    return stdout
+    console.log(`Commits log obtained with sucess for repository ${projectName}`);
+    return stdout;
   
   } catch (error) {
-    console.warn(`Get logs from repository ${repoFullname} failed`)
-    console.warn(`Reasion for failure : ${error.stderr}`)
-    throw error
+    console.warn(`Get logs from repository ${projectName} failed`);
+    console.warn(`Reasion for failure : ${error.stderr}`);
+    throw error;
   }
 }
 
-
-async function getCommits(org, repository) {
-  const repoFullname = `${org}/${repository}`
-  const repoPath = `${REPO_STORAGE}/${repoFullname}`
+async function getCommitsFromScratch(org, repository) {
+  const repoFullname = `${org}/${repository}`;
+  const repoPath = `${REPO_STORAGE}/${repoFullname}`;
   if (fs.existsSync(repoPath) ) {
-    console.log(`Repository ${repoPath} already cloned.`)
+    console.log(`Repository ${repoPath} already cloned.`);
     await pull(org, repository);
   }
   else {
-    await clone(org, repository)
+    await clone(org, repository);
   }
-  const logs = await log(org, repository)
+  const logs = await log(org, repository);
   const commits = logs.split('\n');
   const commitsObjects = commits.map((commit) => {
-    const commitProps = commit.split('_')
+    const commitProps = commit.split('_');
     return {
       hash: commitProps[0],
       author: commitProps[1],
       timestamp: commitProps[2],
       message: commitProps[3]
-    }
-  })
+    };
+  });
 
-  return commitsObjects
+  return commitsObjects;
+}
+
+async function getCommits(rootPath, projectName) {
+  const logs = await log(rootPath, projectName);
+  const commits = logs.split('\n');
+  const commitsObjects = commits.map((commit) => {
+    const commitProps = commit.split('_');
+    return {
+      hash: commitProps[0],
+      author: commitProps[1],
+      timestamp: commitProps[2],
+      message: commitProps[3]
+    };
+  });
+
+  return commitsObjects;
 }
 module.exports = {
-  getCommits
-}
+  getCommits,
+  pull,
+  getCommitsFromScratch,
+  clone
+};
 
